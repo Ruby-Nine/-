11b295161866105a7703de840aca4cc4abb6d1a1
Ruby-Nine <zhb976153@icloud.com>
2024-05-15 22:53:12 +0800

    添加了提示和作品功能

--- show_image.js
+++ show_image.js
@@ -4,22 +4,53 @@ var currImageId = -1;
 var questions = database["questionLists"];
 
 var total = questions.length;
+var difficulty = "";
 
 document.getElementById('showRandomImage').addEventListener('click', function () {
-    showRandomImage();
+    showRandomImage()
 });
-
+document.getElementById('showHint').addEventListener('click', function(){
+    showHint();
+});
+document.getElementById('showOrigin').addEventListener('click', function(){
+    showOrigin();
+});
 document.getElementById('showAnswer').addEventListener('click', function(){
     showAnswer();
 });
 
+
 function showRandomImage(){
     randomImageId();
+    while(difficulty!="" && questions[currImageId]["difficulty"] != difficulty){
+        randomImageId();
+    }
     let randomImageSrc = imagePath + questions[currImageId]["image"];
     document.getElementById("currentImage").src = randomImageSrc;
+    // document.getElementById("hint").value = "";
+    // document.getElementById("origin").value = "";
+    // document.getElementById("answer").value = "";
+}
+
+function showHint(){
+    let hint = questions[currImageId]["hint"];
+    // document.getElementById("hint").value = hint;
+    alert(hint);
+}
+
+function showOrigin(){
+    let origin = questions[currImageId]["origin"];
+    // document.getElementById("origin").value = origin;
+    alert(origin);
 }
 
 function showAnswer(){
+    // showHint();
+    // showOrigin();
+    let origin = questions[currImageId]["origin"];
+    let answer = questions[currImageId]["answer"];
+    alert("出处：" + origin + "\n" + answer);
+    // document.getElementById("answer").value = answer;
 }
 
 function randomImageId(){ 
